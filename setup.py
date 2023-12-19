ea86f4b1166c4b752dc5784ead80c1772ee442c9
root <rondo.lehner@gmail.com>
2023-12-19 19:24:57 +0000

    cookiecutter project template py3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='UoL BSc Computer Science Final Project - Land cover classification on Deep Globe 2018 dataset',
+    author='Rondo Lehner',
+    license='',
+)
