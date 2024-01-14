49aa1e083e51ca16d4a072088f05b1f65b87c2a0
Rondo Lehner <rondo.lehner@gmail.com>
2024-01-14 21:29:09 +0000

    increased buffer_size shuffle

--- src/data/pipelines/convnet_pipeline.py
+++ src/data/pipelines/convnet_pipeline.py
@@ -123,7 +123,7 @@ class ConvnetPipeline():
             .batch(self.batch_size_images)
             .map(lambda x: self.load_patches_labels(x, self.image_size, self.patch_size, self.patch_size_annotation, self.patch_stride), num_parallel_calls=tf.data.AUTOTUNE)
             .unbatch() # Flatten the batches for training
-            .shuffle(buffer_size=10000) # Shuffle entire image batch
+            .shuffle(buffer_size=40000) # Shuffle entire image batch
             .batch(self.batch_size_patches) # Rebatch patches as desired
             .prefetch(buffer_size=tf.data.AUTOTUNE)
         )
